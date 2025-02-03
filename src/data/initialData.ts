bf190850d08794ff360f7ac52bac81bac144be84
Vivek Verma <97779638+vivekverma52@users.noreply.github.com>
2025-02-04 00:43:55 +0530

    Add files via upload

---
+++ src/data/initialData.ts
@@ -0,0 +1,48 @@
+import { ProductLine, Invoice } from './types'
+
+export const initialProductLine: ProductLine = {
+  description: '',
+  quantity: '1',
+  rate: '0.00',
+}
+
+export const initialInvoice: Invoice = {
+  logoWidth: 100,
+  title: '',
+  companyName: 'Madan Gupta Vegetables and fruits Supplier',
+  name: '',
+  companyAddress: '',
+  companyAddress2: '',
+  companyCountry: 'Bhopal',
+  billTo: 'Bill To:',
+  clientName: '',
+  clientAddress: '',
+  clientAddress2: '',
+  invoiceTitleLabel: 'Invoice#',
+  invoiceTitle: '',
+  invoiceDateLabel: 'Invoice Date',
+  invoiceDate: '',
+  invoiceDueDateLabel: 'Due Date',
+  invoiceDueDate: '',
+  productLineDescription: 'Item Description',
+  productLineQuantity: 'Qty',
+  productLineQuantityRate: 'Rate',
+  productLineQuantityAmount: 'Amount',
+  productLines: [
+    {
+      description: 'Brochure Design',
+      quantity: '2',
+      rate: '100',
+    },
+    { ...initialProductLine },
+    { ...initialProductLine },
+  ],
+  subTotalLabel: 'Sub Total',
+  taxLabel: 'Sale Tax (10%)',
+  totalLabel: 'TOTAL',
+  currency: 'RS',
+  notesLabel: 'Notes',
+  notes: 'It was great doing business with you.',
+  termLabel: 'Terms & Conditions',
+  term: 'Please make the payment by the due date.',
+}
